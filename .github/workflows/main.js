a80acc47daa24611046d75736ce107afb3b67a5c
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-01-24 16:56:41 +0700

    Check with file types

--- .github/workflows/main.js
+++ .github/workflows/main.js
@@ -6,24 +6,30 @@ console.info("BẮT ĐẦU THỰC HIỆN");
 //joining path of directory
 const directoryPath = ".\\";
 //passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-   //handling error
-   if (err) {
-      return console.log("Unable to scan directory: " + err);
-   }
-   //listing all files using forEach
-   files.forEach(function (file) {
-      // Do whatever you want to do with the file
-      if (file.isDirectory() === false) {
-         console.log(file);
-         console.log(JSON.stringify(file));
-         fs.readFile(file, "utf8", (err, data) => {
-            if (err) {
-               console.error(err);
-               return;
-            }
-            console.log(data);
-         });
+fs.readdir(
+   directoryPath,
+   {
+      withFileTypes: true,
+   },
+   function (err, files) {
+      //handling error
+      if (err) {
+         return console.log("Unable to scan directory: " + err);
       }
-   });
-});
+      //listing all files using forEach
+      files.forEach(function (file) {
+         // Do whatever you want to do with the file
+         if (file.isFile() === true) {
+            console.log(file);
+            console.log(JSON.stringify(file));
+            fs.readFile(file, "utf8", (err, data) => {
+               if (err) {
+                  console.error(err);
+                  return;
+               }
+               console.log(data);
+            });
+         }
+      });
+   }
+);
