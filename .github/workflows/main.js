0bb108d567b6cb7e023fe9b41859e7380be5a30b
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-01-24 16:44:36 +0700

    Thử list file

--- .github/workflows/main.js
+++ .github/workflows/main.js
@@ -1,4 +1,22 @@
+//requiring path and fs modules
+const path = require("path");
+const fs = require("fs");
+
 async function runcode() {
    console.log("thử thôi");
 }
 runcode();
+//joining path of directory
+const directoryPath = ".\\";
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+   //handling error
+   if (err) {
+      return console.log("Unable to scan directory: " + err);
+   }
+   //listing all files using forEach
+   files.forEach(function (file) {
+      // Do whatever you want to do with the file
+      console.log(file);
+   });
+});
