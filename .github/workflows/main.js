a58f31da7317394608a05e83f887457c15c07ea6
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-01-25 10:54:13 +0700

    .libraryfile.json

--- .github/workflows/main.js
+++ .github/workflows/main.js
@@ -71,32 +71,32 @@ console.info("BẮT ĐẦU THỰC HIỆN");
 //joining path of directory
 const directoryPath = ".\\";
 //passsing directoryPath and callback function
-// fs.readdir(
-//    directoryPath,
-//    {
-//       withFileTypes: true,
-//    },
-//    function (err, files) {
-//       //handling error
-//       if (err) {
-//          return console.log("Unable to scan directory: " + err);
-//       }
-//       //listing all files using forEach
-//       files.forEach(function (file) {
-//          // Do whatever you want to do with the file
-//          if (file.isFile() === true) {
-//             fs.readFile(file.name, "utf8", (err, data) => {
-//                if (err) {
-//                   console.error(err);
-//                   return;
-//                } else if (data) {
-//                   let objFile = JSON.parse(data);
-//                   console.log(objFile);
-//                   //https://github.com/o-ngtrieuhau861gmailcom/oLibraries.meta/raw/main/OTH.TestBuildEvent.dll.libraryfile.json
-//                   var url = "https://github.com/o-ngtrieuhau861gmailcom/oLibraries.meta/blob/main/OTH.TestBuildEvent.dll.libraryfile.json?raw=true";
-//                }
-//             });
-//          }
-//       });
-//    }
-// );
+fs.readdir(
+   directoryPath,
+   {
+      withFileTypes: true,
+   },
+   function (err, files) {
+      //handling error
+      if (err) {
+         return console.log("Unable to scan directory: " + err);
+      }
+      //listing all files using forEach
+      files.forEach(function (file) {
+         // Do whatever you want to do with the file
+         if (file.isFile() === true && file.name.endsWith(".libraryfile.json")) {
+            fs.readFile(file.name, "utf8", (err, data) => {
+               if (err) {
+                  console.error(err);
+                  return;
+               } else if (data) {
+                  let objFile = JSON.parse(data);
+                  console.log(objFile);
+                  //https://github.com/o-ngtrieuhau861gmailcom/oLibraries.meta/raw/main/OTH.TestBuildEvent.dll.libraryfile.json
+                  var url = "https://github.com/o-ngtrieuhau861gmailcom/oLibraries.meta/blob/main/OTH.TestBuildEvent.dll.libraryfile.json?raw=true";
+               }
+            });
+         }
+      });
+   }
+);
